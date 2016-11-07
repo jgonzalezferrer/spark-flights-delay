fd62cc07e564467c984224f4750d0f530d2d6f94
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-11-07 01:56:55 +0000

    Script error fixed

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ cd data
 wget -nc http://stat-computing.org/dataexpo/2009/$archive.csv.bz2 
 bzip2 -dk $archive.csv.bz2
 
-hdfs -dfs -mkdir -p $folder
-hdfs -dfs -put $archive.csv $project
+hdfs dfs -mkdir -p $folder
+hdfs dfs -put $archive.csv $folder
 
 cd ..
