379fc80dd7addb974a6731ba38f65c2c783095bc
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-11-07 06:10:32 +0000

    Filtering forbidden columns.

--- src/load.scala
+++ src/load.scala
@@ -4,7 +4,19 @@ val project = "/project"
 val archive = "2000"
 
 // Read csv file with headers from hdfs
-val flightsDF = spark.read.format("csv").option("header", "true").load("hdfs://"+project+"/"+archive+".csv")
+var flightsDF = spark.read.format("csv").option("header", "true").load("hdfs://"+project+"/"+archive+".csv")
 
 // Print schema
 flightsDF.printSchema
+
+// Forbidden variables
+val forbiddenVariables = Array("ArrTime", "CarrierDelay", "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay")
+
+// Creating allowed variables extracting the forbidden variables from the original ones.
+val allowedVariables = flightsDF.columns.filter(x => !forbiddenVariables.contains(x))
+// Convert them into column format.
+val allowedColumns = allowedVariables.map(x => col(x))
+
+// Selecting the just the allowed columns.
+flightsDF = flightsDF.select(allowedColumns:_*)
+
