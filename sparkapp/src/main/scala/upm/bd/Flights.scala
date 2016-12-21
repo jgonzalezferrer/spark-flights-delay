1739fcb374ababb0e597ba4b2ed718e6029cbbf9
A. Javier González Ferrer <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-21 19:40:27 +0100

    Import missing libraries.

--- sparkapp/src/main/scala/upm/bd/Flights.scala
+++ sparkapp/src/main/scala/upm/bd/Flights.scala
@@ -4,6 +4,16 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
+import org.apache.spark.ml.feature.OneHotEncoder
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 
 class Flights(spark: SparkSession) {
 
@@ -40,7 +50,7 @@ class Flights(spark: SparkSession) {
 				col("CancellationCode").cast(StringType))
 	}
 
-	def variableTransformations(){
+	def variablesTransformation(){
 		//Convert scheduled departure and arrival time to TimeStamp
 		df = dateToTimeStamp(df, "CRSDepTime")
 		df = dateToTimeStamp(df, "CRSArrTime")
