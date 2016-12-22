9ee8cfc8156de9a2407aa72477bfc511866751e8
A. Javier González Ferrer <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-22 16:23:19 +0100

    Final modeçl

--- sparkapp/src/main/scala/upm/bd/App.scala
+++ sparkapp/src/main/scala/upm/bd/App.scala
@@ -4,44 +4,69 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.VectorIndexer
-import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
-import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
+import org.apache.spark.ml.regression.LinearRegressionModel
 import org.apache.spark.ml.feature.OneHotEncoder
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.ml.PipelineModel
+
+/** Spark application that creates and compares machine learning models from MLlib for predicting the arrival delay of USA commercial flights.
+ * 
+ *  The development of this application follows the usual data science pipeline:
+ *	- Load and process input data.
+ * 	- Select, transform and manipulate the input variables, to prepare them for the ML models. 
+ *	- Create and compare machine learning model that predicts the arrival delay time.
+ *  - Validate the created model and provide some measure of its accuracy.
+ *
+ *	@param: the HDFS location of the dataset(s).
+ *	@author: Antonio Javier González Ferrer
+ *	@author: Aitor Palacios Cuesta
+ */
 
 object App {
 	def main(args : Array[String]) {
 
+	/* Configuration 
+	 *
+	 * Defining some Spark configuration parameters.
+	 *
+	 */
+
 	// Disabling debug option. 
 	Logger.getRootLogger().setLevel(Level.WARN)
-
+	// Entry point to programming Spark with the Dataset and DataFrame API.
 	val spark = SparkSession
 			.builder()
 			.appName("Spark Flights Delay")
-			.getOrCreate()
- 
+			.getOrCreate() 
+ 	// Implicit methods available in Scala for converting Scala objects into DataFrames.
 	import spark.implicits._
 
-	val targetVariable = "ArrDelay"
-	var flights = new Flights(spark, targetVariable)
 
+	/* Loading data
+	 *
+	 * We load the data from the HDFS, a highly fault-tolerant distributed file system. 
+	 *
+	 */
+
+	val targetVariable = "ArrDelay"
 	val datasetsPath = args(0)
+	// Create a Flights object, where the DataFrame and the Machine Learning methods will be stored.
+	var flights = new Flights(spark, targetVariable)	
 	flights.load("hdfs://"+datasetsPath+"*.csv")
 	
-	/* Discarding data points */
-	//Drop rows with null values in the target variable	
+
+	/* Data Manipulation
+	 *
+	 * The dataset contains 29 initial variables. Some of these variables are not allowed to be used in this ML problem.
+	 * Furthermore, some of the varibles need to be transformated, cleaned or discarded.
+	 *
+	 */
+	//Drop rows with null values in the target variable	(not allowed in supervised learning).
 	flights.df = flights.df.na.drop(Array("ArrDelay"))	
 
-	/* Transformation of variables */
+	// Transformation of variables for the learning phase. 
 	flights.variablesTransformation()
 
-	/* Adding new variables */	
+	// Adding new varibles: lat and long of each airport.	
 	val airportsDF = spark.read
 		.format("com.databricks.spark.csv")
 		.option("header", "true")
@@ -56,7 +81,7 @@ object App {
 				.withColumnRenamed("long", "OriginLong")
 				.drop("iata")
 
-				
+	// New columns: lat and long of the Dest airports.	
 	flights.df = flights.df.join(airportsDF, flights.df("Dest") === airportsDF("iata"))
 				.withColumnRenamed("lat", "DestLat")
 				.withColumnRenamed("long", "DestLong")
@@ -70,31 +95,39 @@ object App {
 						.drop("Origin").drop("Dest")
 
 
-	/* Null treatment */
+	// Null treatment.
 	// We discard all the rows with at least one null value since they represent a reasonably low amount (<1%).
 	flights.df = flights.df.na.drop()
 
 	// We will take the standard deviation to use it as a baseline.
-	// We will compare the other methods againts this naive method.
+	// We will compare the other methods against this naive method.
 	val dStDev = flights.df.select(stddev("ArrDelay")).take(1)(0)(0)
 	
-	/* Modification of variable to use them in the regression part */
+	// Linear Regression method needs to define a special transformation for categorical variables.
 	//OneHotEncoder to create dummy variables for carrier, month and day of the week 
 	val dayEncoder = new OneHotEncoder().setInputCol("DayOfWeek").setOutputCol("dummyDayOfWeek")
 	val monthEncoder = new OneHotEncoder().setInputCol("Month").setOutputCol("dummyMonth")
 	val carrierEncoder = new OneHotEncoder().setInputCol("UniqueCarrierInt").setOutputCol("dummyUniqueCarrier")
 
-	// Just for regression
-	//Linear regression needs these variables to handle those categorical variables properly.
 	flights.df = dayEncoder.transform(flights.df)
 	flights.df = monthEncoder.transform(flights.df)
 	flights.df = carrierEncoder.transform(flights.df)
 
-	/* Training and Test dataset */
-	// Split the data into training and test sets (30% held out for testing).
-	var Array(trainingData, testData) = flights.df.randomSplit(Array(0.7, 0.3), 100) // last parameter is the seed
 
-	/* Linear Regression */
+	/* Machine Learning methods
+	 *
+	 * We will choose three supervised machine learning to predict the arrival delay:
+	 * - Linear Regression
+	 * - Random Forest
+	 * - Boosting Trees
+	 * 
+	 */
+
+	// Training and Test datasets
+	// Split the data into training and test sets (30% held out for testing).
+	var Array(trainingData, testData) = flights.df.randomSplit(Array(0.7, 0.3))
+
+	// We need to modify the variables in Linear Regression, converting the categorical into OneHotEncoder.
 	// For regression, we will use the OneHotEncoder variables, for Random Forests and Boosting Trees we will use the original ones.
 	var trainingDataR = trainingData
 	var testDataR = testData
@@ -105,37 +138,87 @@ object App {
 	testDataR = testDataR.drop("DayOfWeek")
 							.drop("Month").drop("UniqueCarrierInt")
 
-	// Training the model
-	flights.linearRegression(trainingDataR, 100, 1, 3, Array(0.1, 1.0, 10.0))	
-	val lrModel = flights.linearRegressionModel.fit(trainingDataR)
-
-	// Retrieving the best model tuning selection.
-	val pipeline = lrModel.bestModel.asInstanceOf[PipelineModel]
-	val bestRegularizer = pipeline.stages(1).asInstanceOf[LinearRegressionModel].getRegParam
-
-	val lrPredictions = lrModel.transform(testDataR)
-	val rmseRegression = flights.evaluator.evaluate(lrPredictions)
-
-
 	// For Random forest and bosting trees, we discard the OneHotEncoder variables, keeping the old ones.
 	trainingData = trainingData.drop("dummyDayOfWeek")
 							.drop("dummyMonth").drop("dummyUniqueCarrierInt")
 	testData = testData.drop("dummyDayOfWeek")
 							.drop("dummyMonth").drop("dummyUniqueCarrierInt")
 
-	/* Random Forest */
-	flights.randomForest(trainingData, 15)
+
+	/* Linear Regression Model
+	 *	
+	 * We would like to tune the regularisaiton hyperparameter in order to avoid overfitting. We perform a grid search
+     * of three parameters using 3-fold cross-validation to find out the best possible combination of parameters.
+	 *
+	 * On the other hand, the max number of iterations will be 100 since it usually converges quite fast, 
+	 * and the elastic net parameter is set to 1 (Lasso regularisation).
+	 */
+
+	val lrMaxNumIterations = 100
+	val elasticNetParameter = 1
+	val k = 3
+	regularisations = Array(0.1, 1.0, 10.0)
+	flights.linearRegression(trainingDataR, lrMaxNumIterations, elasticNetParameter, k, regularisations)
+
+	// Training the model	
+	val lrModel = flights.linearRegressionModel.fit(trainingDataR)
+
+	// Retrieving the best model of tuning selection.
+	val pipeline = lrModel.bestModel.asInstanceOf[PipelineModel]
+	val bestRegularizer = pipeline.stages(1).asInstanceOf[LinearRegressionModel].getRegParam
+
+	// Validation
+	val lrPredictions = lrModel.transform(testDataR)
+	val rmseRegression = flights.evaluator.evaluate(lrPredictions)
+
+	/* Random Forest Model
+	 * 
+	 * Usually works well in the presence of categorical variables. 
+	 *
+	 * The max number of categories to be considered as a categorical variable is set to 15.
+	 *
+	 */
+	
+	val rfMaxCategories = 15
+	flights.randomForest(trainingData, rfMaxCategories)
+
+	// Training the model
 	val rfModel = flights.randomForestModel.fit(trainingData)
+
+	// Validation
 	val rfPredictions = rfModel.transform(testData)
 	val rmseRandom = flights.evaluator.evaluate(rfPredictions)
 
-	/* Boosting trees */
-	flights.boostingTrees(trainingData, 15, 10)
+	/* Boosting Trees Model
+	 *
+	 * We do not choose random selection of the feature to split the data node, as in Random Forest.
+	 *
+	 * The max number of categories to be considered a categorical variable is set to 15.
+	 * The max number of iterations is 10.
+	 */
+
+	val btMaxCategories = 15
+	val btMaxNumIterations = 10
+	flights.boostingTrees(trainingData, btMaxCategories, btMaxNumIterations)
+
+	// Training the model
 	val btModel = flights.boostingTreesModel.fit(trainingData)
+
+	// Validating the model
 	val btPredictions = btModel.transform(testData)
 	val rmseBoosting = flights.evaluator.evaluate(btPredictions)
 
+
+	/* Validation
+	 *
+	 * We decide to use the root-mean-square error (RMSE) as the accuracy measure.
+	 *
+	 */
+
+	// Baseline to improve
 	println("Standard deviation of arrival delay = "+dStDev)
+
+	// RMSE of the different ML techniques.
 	println("rmse for different algorithms: ")
 	println("Linear regression with best regularizer: "+bestRegularizer+" = "+rmseRegression)
 	println("Random forests = "+rmseRandom)
--- sparkapp/src/main/scala/upm/bd/Flights.scala
+++ sparkapp/src/main/scala/upm/bd/Flights.scala
@@ -1,46 +1,58 @@
 package upm.bd
 
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.Column
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.VectorIndexer
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
-import org.apache.spark.ml.feature.OneHotEncoder
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+
+/** This class models and manipulates the USA commercial flights dataset to train some machine learning methods.
+ * 
+ *  The Flights class stores a DataFrame of flights, which can be modified in order to use it as input of machine learning methods.
+ *	Three machine learning methods are currently supported provided by the MLlib library:
+ *	- Linear Regression Model
+ *  - Random Forests
+ *  - Boosting Trees
+ *
+ *	@param: spark, a Spark session 
+ *  @param: targetVariable, the target variable to predict.
+ *	@author: Antonio Javier González Ferrer
+ *	@author: Aitor Palacios Cuesta
+ */
 
 class Flights(spark: SparkSession, targetVariable: String) {
 
 	import spark.implicits._
 
 	var df: DataFrame = null
-	var linearRegressionModel: CrossValidator = null
 	var evaluator: RegressionEvaluator = null
-	var assembler: VectorAssembler = null
+	var linearRegressionModel: CrossValidator = null	
 	var randomForestModel: Pipeline = null
 	var boostingTreesModel: Pipeline = null
 
-	//Evaluating the result
+	//Evaluator defined for the ML methods using the RMSE metric.
 	evaluator = new RegressionEvaluator()
 				.setLabelCol(targetVariable)
 				.setPredictionCol("prediction")
 				.setMetricName("rmse")
 
-
-	// Read all csv files with headers from hdfs.
-	// The valid columns are selected, casting them (the default type is String).
+	/* Read all csv files with headers from HDFS.
+	 *
+	 * The valid columns are selected, casting them to their correct type (the default type is String).
+	 * 
+	 * @param: hdfsPath, the hdfs path of the datasets.
+	 */
 	def load(hdfsPath: String){
 		df = spark.read
 			.format("com.databricks.spark.csv")
 			.option("header", "true")
-			//.load("hdfs://"+args(0)+"*.csv")
 			.load(hdfsPath)
 			.select(col("Year").cast(StringType),
 				col("Month").cast(StringType),
@@ -61,20 +73,27 @@ class Flights(spark: SparkSession, targetVariable: String) {
 				col("TaxiOut").cast(DoubleType),
 				col("Cancelled").cast(BooleanType),
 				col("CancellationCode").cast(StringType))
-
 	}
 
-	/* 
-	* Transform a date in "dd/MM/yyyy HHmm" format to Unix TimeStamp 
-	* Input: columnName -> name of the column to transform
-	* Return: new dataframe
-	*/
+	/* Transform a column date in "dd/MM/yyyy HHmm" format to a Unix TimeStamp column
+	 * 
+	 * @param: df, the original dataframe
+	 * @param: columnName, the column name to transform.
+	 * @return: new dataframe with the TimeStamp column.
+	 */
 	def dateToTimeStamp(df: org.apache.spark.sql.DataFrame, columnName: String) : org.apache.spark.sql.DataFrame = {
 	return df.withColumn(columnName, 
 		unix_timestamp(concat(col("DayOfMonth"), lit("/"), col("Month"), lit("/"), col("Year"), lit(" "), col(columnName)), 
 			"dd/MM/yyyy HHmm"))
 	}  
 
+	/* Transformation of initial variables to be suitable for the learning phase.
+	 *
+	 * - CRSDepTime and CRSArrTime are converted to TimeStamp.
+	 * - Numerical variables changed to Double variables due to regression models limitations.
+	 * - UniqueCarrier variable from String to Categorical using StringIndexer.
+	 *
+	 */
 	def variablesTransformation(){
 		//Convert scheduled departure and arrival time to TimeStamp
 		df = dateToTimeStamp(df, "CRSDepTime")
@@ -85,7 +104,7 @@ class Flights(spark: SparkSession, targetVariable: String) {
 		df = df.withColumn("CRSDepTime", $"CRSDepTime" - timeStampReference)
 		df = df.withColumn("CRSArrTime", $"CRSArrTime" - timeStampReference)
 
-		//Cast variables to Double deu to machine learning methods restrictions.
+		//Cast variables to Double due to machine learning methods restrictions.
 		df = df.withColumn("DayOfMonth", col("DayOfMonth").cast(DoubleType))
 		df = df.withColumn("CRSDepTime", col("CRSDepTime").cast(DoubleType))
 		df = df.withColumn("CRSArrTime", col("CRSArrTime").cast(DoubleType))
@@ -95,47 +114,59 @@ class Flights(spark: SparkSession, targetVariable: String) {
 		//StringIndexer to transform the UniqueCarrier string to integer for using it as a categorical variable.
 		val sIndexer = new StringIndexer().setInputCol("UniqueCarrier").setOutputCol("UniqueCarrierInt")
 		df = sIndexer.fit(df).transform(df)
-
 	}
 
-	def linearRegression(trainingData: DataFrame, maxIter: Int, elasticNetParameter: Int, k: Int, hyperparameters: Array[Double]){ 
-	
-		//Defining the model
+	/* Linear Regression method.
+	 *
+	 * @param: trainingData, the training data.
+	 * @param: maxIter, max. number of iterations (if it does not converge before).
+	 * @param: elasticNetParameter, the elastic net parameter from 0 to 1.
+	 * @param: k, the number of folds in cross validation.
+	 * @param: hyperaparameters, set of regularizer variables to be tune by cross validation.
+	 */
+	def linearRegression(trainingData: DataFrame, maxIter: Int, elasticNetParameter: Int, k: Int, hyperparameters: Array[Double]){ 	
 		//Prepare the assembler that will transform the remaining variables to a feature vector for the ML algorithms
-		assembler = new VectorAssembler()
+		val assembler = new VectorAssembler()
 				.setInputCols(trainingData.drop(targetVariable).columns)
 				.setOutputCol("features")
 
+		// Defininf the model.
 		val lr = new LinearRegression()
 			.setFeaturesCol("features")
 			.setLabelCol(targetVariable)
 			.setMaxIter(maxIter)
 			.setElasticNetParam(elasticNetParameter)
 
-		//Preparing the pipeline
+		//Preparing the pipeline to train and test the data with the regression algorithm.
 		val regressionPipeline = new Pipeline().setStages(Array(assembler, lr))
 
-		//To tune the parameters of the model
+		//To tune the parameters of the model.
 		var paramGrid = new ParamGridBuilder()
 			.addGrid(lr.getParam("regParam"), hyperparameters)
 			.build()
 
+		// Cross validation to select the best hyperparameter.
 		linearRegressionModel = new CrossValidator()
 			.setEstimator(regressionPipeline)
 			.setEvaluator(evaluator)
 			.setEstimatorParamMaps(paramGrid)
 			.setNumFolds(k)
 	}
-	
+
+	/* Random Forest method.
+	 *
+	 * @param: trainingData, the training data.
+	 * @param: maxCategories, the max number of different categories to be considered as a categorical variable.
+	 */
 	def randomForest(trainingData: DataFrame, maxCategories: Int){
 
-		//Prepare the assembler that will transform the remaining variables to a feature vector for the ML algorithms
-		assembler = new VectorAssembler()
+		//Prepare the assembler that will transform the remaining variables to a feature vector for the ML algorithms.
+		val assembler = new VectorAssembler()
 				.setInputCols(trainingData.drop(targetVariable).columns)
 				.setOutputCol("features")
 
-		//Vector Indexer to indicate that some variables are categorical, so they are treated properly by the algorithms
-		//In our case, DayOfWeek, Month, UniqueCarrier have less than 15 different classes, so they will be marked as categorical, as we want
+		//Vector Indexer to indicate that some variables are categorical, so they are treated properly by the algorithms.
+		//In our case, DayOfWeek, Month, UniqueCarrier have less than 15 different classes, so they will be marked as categorical, as we want.
 		var indexer = new VectorIndexer()
 			.setInputCol("features")
 			.setOutputCol("indexed")
@@ -146,14 +177,19 @@ class Flights(spark: SparkSession, targetVariable: String) {
 			.setLabelCol(targetVariable)
 			.setFeaturesCol("features")
 
-		randomForestModel = new Pipeline().setStages(Array(assembler, indexer, rf))
-		
+		//Pipeline to train and test the data with the random forest.
+		randomForestModel = new Pipeline().setStages(Array(assembler, indexer, rf))	
 	}
 
+	/* Boosting Trees method.
+	 *
+	 * @param: trainingData, the training data.
+	 * @param: maxCategories, the max number of different categories to be considered as a categorical variable.
+	 * @param: maxIter, the max number of iterations.
+	 */
 	def boostingTrees(trainingData: DataFrame, maxCategories: Int, maxIter: Int){
-
 		//Prepare the assembler that will transform the remaining variables to a feature vector for the ML algorithms
-		assembler = new VectorAssembler()
+		val assembler = new VectorAssembler()
 				.setInputCols(trainingData.drop(targetVariable).columns)
 				.setOutputCol("features")
 
@@ -169,8 +205,5 @@ class Flights(spark: SparkSession, targetVariable: String) {
 
 		//Pipeline to train and test the data with the boosting algorithm
 		boostingTreesModel = new Pipeline().setStages(Array(assembler, indexer, gbt))
-	
 	}
-
 }
-
