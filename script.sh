9f8f75c9a8bb62439f225efdb2008681111a9537
A. Javier González Ferrer <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-22 21:02:12 +0100

    Correct script to generate airport dataset.

--- script.sh
+++ script.sh
@@ -1,21 +1,7 @@
-archive=2000
 folder=/project
 
-mkdir -p data
-mkdir -p data/flights
-mkdir -p data/extra
-cd data/flights
-
-wget -nc http://stat-computing.org/dataexpo/2009/$archive.csv.bz2 
-bzip2 -dk $archive.csv.bz2
-
-cd ..
-cd extra
 wget -nc http://stat-computing.org/dataexpo/2009/airports.csv
-cd ..
 
 hdfs dfs -mkdir -p $folder 
-hdfs dfs -mkdir -p $folder/flights 
 hdfs dfs -mkdir -p $folder/extra 
-hdfs dfs -put ./flights/$archive.csv $folder/flights
-hdfs dfs -put ./extra/airports.csv $folder/extra
+hdfs dfs -put airports.csv $folder/extra
