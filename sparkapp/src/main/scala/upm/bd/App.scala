1cc7d400f8a147bcf60eaee1af33c058adc66d26
A. Javier González Ferrer <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-22 12:27:48 +0100

    Import libraries and added csv extension.

--- sparkapp/src/main/scala/upm/bd/App.scala
+++ sparkapp/src/main/scala/upm/bd/App.scala
@@ -7,12 +7,12 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.VectorIndexer
-import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
 import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
 import org.apache.spark.ml.feature.OneHotEncoder
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
-import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 
 object App {
@@ -31,8 +31,8 @@ object App {
 	val targetVariable = "ArrDelay"
 	var flights = new Flights(spark, targetVariable)
 
-	val datasetsPath = args[0]
-	flights.load("hdfs://"+datasetsPath)
+	val datasetsPath = args(0)
+	flights.load("hdfs://"+datasetsPath+"*.csv")
 	
 	/* Discarding data points */
 	//Drop rows with null values in the target variable	
