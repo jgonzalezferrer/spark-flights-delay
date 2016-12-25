21647027763be15fb0e0cb2ac12462f08137b1c1
A. Javier González Ferrer <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-26 00:59:07 +0100

    Restructure files distribution.

--- app/script.sh
+++
@@ -1,7 +0,0 @@
-folder=/project
-
-wget -nc http://stat-computing.org/dataexpo/2009/airports.csv
-
-hdfs dfs -mkdir -p $folder 
-hdfs dfs -mkdir -p $folder/extra 
-hdfs dfs -put airports.csv $folder/extra
